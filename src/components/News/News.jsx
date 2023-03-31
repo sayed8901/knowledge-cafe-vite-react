062ffe0073722217ced631e925a6c7e234441c73
sayed8901 <sayed91515@gmail.com>
2023-03-31 20:04:57 +0600

    blog fatched & displayed

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import './News.css'
+import './News.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookBookmark, faBookmark } from '@fortawesome/free-solid-svg-icons';
 
 const News = (props) => {
-    const {id, blogTitle, coverImage, authorThumbnail, publishedDate, readTimeInMinutes} = props.newsItem;
+    const {id, blogTitle, coverImage, authorThumbnail, author, publishedDate, readTimeInMinutes} = props.newsItem;
     return (
-        <div>
-            <h2>news title: {blogTitle}</h2>
+        <div className='news'>
+            <img src={coverImage} alt="" />
+            <div className='flex'>
+                <div className='flex1'>
+                    <img src={authorThumbnail} alt="" />
+                    <div className='author-info'>
+                        <h4>{author}</h4>
+                        <p>{publishedDate}</p>
+                    </div>
+                </div>
+                <div className='flex1'>
+                    <p>{readTimeInMinutes} min read</p>
+                    <FontAwesomeIcon icon={faBookmark} />
+                </div>
+            </div>
+            <h2>{blogTitle}</h2>
         </div>
     );
 };
