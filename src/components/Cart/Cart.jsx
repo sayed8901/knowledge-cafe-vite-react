c5893ccf556c71befcae8d78f8623300704a4f36
sayed8901 <sayed91515@gmail.com>
2023-04-01 01:01:19 +0600

    total spent time integrated with localStorage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Cart.css'
 
 const Cart = (props) => {
     const readTime = props.readTime;
+
+    const [time, setTime] = useState(readTime);
+
+    useEffect( () => {
+        const getTotalReadTime = localStorage.getItem('readTime');
+        setTime(getTotalReadTime);
+
+    } , [readTime])
+    
     return (
         <div className='cart-container'>
             <div className='time-spent'>
-                <h3>Spent time on read: {readTime} min</h3>
+                <h3>Spent time on read: {time} min</h3>
             </div>
             <div className='cart-panel'>
                 <h3>Bookmarked Blogs: </h3>
