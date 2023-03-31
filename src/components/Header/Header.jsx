28f129bf26e2865f48ada57f923a3a477bf9b5e7
sayed8901 <sayed91515@gmail.com>
2023-03-31 17:12:37 +0600

    header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../assets/photo.avif'
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <h2>Knowledge Hub</h2>
+            <div className='links'>
+                <div>
+                    <a href="/home">Home</a>
+                    <a href="/contact">Contact</a>
+                    <a href="/login">Log in</a>
+                </div>
+                <img src={logo} alt="" />
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
