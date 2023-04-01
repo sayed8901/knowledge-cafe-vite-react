a9dd1037ca45834d6282846178711a818ac6b3a0
sayed8901 <sayed91515@gmail.com>
2023-04-01 21:47:39 +0600

    mobile landscape responsiveness modified.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -18,14 +18,18 @@ const Cart = (props) => {
     } , [readTime])
     
 
+
     // cart part:
     
+    // receiving 'cart' function from its parent 'Main.jsx' component via props
     const cart = props.cart;
 
+    // 'use State' to handle total 'blogs data saved on local storage' working along with 'useEffect'
     const [blog, setBlog] = useState([]);
 
     const getBlogsFromLocalStorage = JSON.parse(localStorage.getItem('blogTitle'));
 
+    // to handle auto effect-updating the 'cart data' on reload using 'useEffect'
     useEffect( () => {
         if(getBlogsFromLocalStorage){
             setBlog(getBlogsFromLocalStorage);
@@ -37,14 +41,15 @@ const Cart = (props) => {
     return (
         <div className='cart-container'>
             <div className='time-spent'>
-                {/* use here 'updatedTime' get from 'useState' above declared */}
+                {/* use here 'updatedTime' get from 'useState' above declared to get 'updated read time' */}
                 <h3>Spent time on read: {updatedTime} min</h3>
             </div>
             <div className='cart-panel'>
+                {/* use here 'blog.length' get from 'useState' above declared to get 'no of bookmarks already saved on cart' */}
                 <h3>Bookmarked Blogs: {blog.length}</h3>
-                {/* <ShowBlogTitle cart = {cart}></ShowBlogTitle> */}
                 <div>
                     {
+                        // calling 'ShowBlogTitle' component to display the saved blog title here 
                         blog.map(news => (
                             <ShowBlogTitle 
                                 blog = {news}
